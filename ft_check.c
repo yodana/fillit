1d106044fefd3c2db5b042bc3b6d7bd0ed91e02d
Arthur BOCQUINY <arbocqui@e2r12p8.42.fr>
2019-02-16 18:28:30 +0100

    creation branche

--- ft_check.c
+++ ft_check.c
@@ -6,16 +6,22 @@
 /*   By: yodana <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/10 06:14:26 by yodana            #+#    #+#             */
-/*   Updated: 2019/02/16 17:04:40 by yodana           ###   ########.fr       */
+/*   Updated: 2019/02/16 18:27:40 by arbocqui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdio.h>
+
 void	ft_error()
 {
 	ft_putstr("error");
 	exit(0);
+}
+
+void	ft_check_block(int i, int j, char **map)
+{
+	
 }
 
 int			ft_end_block(int i)
