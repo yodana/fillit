1825c1f784f3e420a5b7b4afabc0fa032c5b5893
Yohann DANA <yodana@e1r8p7.42.fr>
2019-02-26 23:30:21 +0100

    recursive en cours

--- ft_resolv.c
+++ ft_resolv.c
@@ -6,52 +6,80 @@
 /*   By: yodana <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 18:58:50 by yodana            #+#    #+#             */
-/*   Updated: 2019/02/21 16:33:18 by yodana           ###   ########.fr       */
+/*   Updated: 2019/02/26 23:30:07 by yodana           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <stdio.h>
-void	ft_resolv(t_final_map *final_map, t_tetris **piece, int x, int y)
+
+char	**ft_put_piece(char **sol, char **piece, int x, int y)
 {
-	t_tetris *tmp;
-	tmp = *piece;
-	while(tmp)
-	{
-		printf("lol");
-		tmp = tmp->next;
-	}
-	char **tetris = tmp->map;
-	char **sol = final_map->sol;
 	int x_t = 0;
 	int y_t = 0;
-	//if(sol[x][y] == '.')
-	//{
-		while(tetris[x_t])
+
+	while (piece[x_t][y_t] != '#')
+	{
+		y_t++;
+		if (!(piece[x_t][y_t]))
 		{
-	//		printf("%s\n",tetris[x_t]);
-			while (tetris[x_t][y_t])
-			{
-				if (tetris[x_t][y_t] == '#' && sol[x][y] != '#')
-					sol[x][y] = '#';
-				y_t++;
-				y++;
-			}
 			y_t = 0;
-			y = 0;
 			x_t++;
-		//	printf("%s\n",sol[x]);
+		}
+	}
+	while (piece[x_t])
+	{
+		while (piece[x_t][y_t] && sol[x][y])
+		{
+			if (piece[x_t][y_t] == '#' && sol[x][y] == '.')
+				sol[x][y] = '#';
+			if (piece[x_t][y_t] == '#' && sol[x][y] == '#')
+			{
+				printf("lol");
+				return (NULL);
+			}
+			y_t++;
+			y++;
+		}
+		if (piece[x_t][y_t] == '\0')
+		{
+			y_t = 0;
+			x_t++;
+		}
+		if (sol[x][y] == '\0')
+		{
+			y = 0;
 			x++;
 		}
-		final_map->sol = sol;
-		piece = &tmp;
-	//	if(tmp->next)	
-	//		ft_resolv(final_map,&(tmp->next), 0, 0);
+	}
+	return (sol);
+
+}
+void	ft_resolv(t_final_map *final_map, t_tetris *piece, int x, int y)
+{
+	char **sol = final_map->sol;
+	char **tetris = piece->map;
+
+	if (sol[x][y] == '#' && y < 4)
+			ft_resolv(final_map, piece, x, y + 1);
+	if (sol[x][y] == '#' && y >= 4)
+		ft_resolv(final_map, piece, x + 1, 0);
+	//int x_t = 0;
+	//int y_t = 0;
+	if ((sol = ft_put_piece(sol,tetris, x, y)) != NULL)
+	{
+		if (piece->next)
+			ft_resolv(final_map, piece->next, 0, 0);
+	}
+	else
+		ft_resolv(final_map, piece, x , y + 1);
 		x = 0;
-		/*while (x < 4)
+		while (x < 4)
 		{
-			printf("%s\n",sol[x]);
+			printf("%s\n",((char**)final_map->sol)[x]);
 			x++;
-		}*/
+		}
+	x = 0;
+		y = 0;
 	//}
 }
